fa7365bf6fb4c36e8ac54f25551022a35b57c2ff
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-25 17:46:44 +0900

    [finish]lesson_2

--- Main.rb
+++ Main.rb
@@ -4,3 +4,16 @@ for i in 5..10
 end
 puts "last #{i}"
 
+# whileによるループ処理
+i = 1
+while i <= 5
+  puts "ハロー、paizaラーニング"
+  i = i + 1
+end
+
+# whileによるループ処理
+i = 1
+while i <= 15
+    puts i
+    i = i + 1
+end
